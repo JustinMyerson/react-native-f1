98e418e3b7a46c3a7c14440bcea071c611a41a9b
JustinMyerson <100346942+JustinMyerson@users.noreply.github.com>
2022-05-26 14:12:55 +0200

    Add Icons to Navigation

--- App.js
+++ App.js
@@ -5,6 +5,13 @@ import CircuitsScreen from "./screens/CircuitsScreen";
 import ConstructorsScreen from "./screens/ConstructorsScreen";
 import DriversScreen from "./screens/DriversScreen";
 import ResultsScreen from "./screens/ResultsScreen";
+import {
+  Feather,
+  SimpleLineIcons,
+  AntDesign,
+  Foundation,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
@@ -16,11 +23,56 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Circuits" component={CircuitsScreen} />
-        <Tab.Screen name="Constructors" component={ConstructorsScreen} />
-        <Tab.Screen name="Drivers" component={DriversScreen} />
-        <Tab.Screen name="Results" component={ResultsScreen} />
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color, size }) => (
+              <Feather name="home" size={24} color="black" />
+            ),
+          }}
+        ></Tab.Screen>
+        <Tab.Screen
+          name="Circuits"
+          component={CircuitsScreen}
+          options={{
+            tabBarLabel: "Circuits",
+            tabBarIcon: ({ color, size }) => (
+              <SimpleLineIcons name="directions" size={24} color="black" />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Constructors"
+          component={ConstructorsScreen}
+          options={{
+            tabBarLabel: "Constructors",
+            tabBarIcon: ({ color, size }) => (
+              <AntDesign name="team" size={24} color="black" />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Drivers"
+          component={DriversScreen}
+          options={{
+            tabBarLabel: "Drivers",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="steering" size={24} color="black" />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Results"
+          component={ResultsScreen}
+          options={{
+            tabBarLabel: "Results",
+            tabBarIcon: ({ color, size }) => (
+              <Foundation name="results" size={24} color="black" />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
