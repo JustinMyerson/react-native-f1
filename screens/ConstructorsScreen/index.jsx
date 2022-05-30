58f2cf222b17c1539df822259986a3c118c4dd12
JustinMyerson <100346942+JustinMyerson@users.noreply.github.com>
2022-05-30 10:28:13 +0200

    Populate constructors screen

--- screens/ConstructorsScreen/index.jsx
+++ screens/ConstructorsScreen/index.jsx
@@ -1,11 +1,76 @@
-import react from "react";
-import { View, Text } from "react-native";
+import react, { useState, useEffect } from "react";
+import { render } from "react-dom";
+import {
+  SafeAreaView,
+  ActivityIndicator,
+  View,
+  Text,
+  FlatList,
+} from "react-native";
+import { styles } from "./style";
 
 const ConstructorsScreen = ({ navigation }) => {
-  return (
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [constructors, setConstructors] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    // Reset the error in case we had one last time we
+    // did an api call
+    setError(null);
+    fetch("http://ergast.com/api/f1/constructors.json")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setConstructors(result.MRData.ConstructorTable.Constructors);
+          setIsLoading(false);
+        },
+        (error) => {
+          setIsLoading(false);
+          setError(error);
+        }
+      );
+  }, []);
+
+  const Constructor = ({ name, nationality }) => (
     <View>
-      <Text>Test</Text>
+      <Text style={styles.constructor}>
+        {name} {nationality}
+      </Text>
     </View>
+  );
+
+  const renderItem = ({ item }) => {
+    return <Constructor name={item.name} nationality={item.nationality} />;
+  };
+
+  if (isLoading) {
+    return (
+      <ActivityIndicator
+        style={{ marginTop: 20 }}
+        size="large"
+        color="#039dfc"
+      />
+    );
+  }
+
+  if (error) {
+    return (
+      <View>
+        <Text>Error: {error.message}</Text>
+      </View>
+    );
+  }
+
+  return (
+    <SafeAreaView style={styles.constructorsList}>
+      <FlatList
+        data={constructors}
+        renderItem={renderItem}
+        keyExtractor={(Constructor) => Constructor.constructorId}
+      />
+    </SafeAreaView>
   );
 };
 
---
+++ screens/ConstructorsScreen/style.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  constructorsList: {
+    marginLeft: 10,
+    marginTop: 10,
+  },
+
+  constructor: {
+    marginBottom: 5,
+  },
+});
