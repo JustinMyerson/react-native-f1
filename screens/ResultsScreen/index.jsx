50e33d84c544edb77d54d555ab40996e415f6d2c
JustinMyerson <100346942+JustinMyerson@users.noreply.github.com>
2022-05-30 12:30:38 +0200

    Populate results screen

--- screens/ResultsScreen/index.jsx
+++ screens/ResultsScreen/index.jsx
@@ -1,11 +1,90 @@
-import react from "react";
-import { View, Text } from "react-native";
+import react, { useState, useEffect } from "react";
+import { render } from "react-dom";
+import {
+  SafeAreaView,
+  ActivityIndicator,
+  View,
+  Text,
+  FlatList,
+} from "react-native";
+import { styles } from "./style";
 
 const ResultsScreen = ({ navigation }) => {
-  return (
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [results, setResults] = useState([]);
+  const [resultsArray, setResultsArray] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    // Reset the error in case we had one last time we
+    // did an api call
+    setError(null);
+    fetch("https://ergast.com/api/f1/current/last/results.json")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setResultsArray(result.MRData.RaceTable.Races[0].Results);
+          setResults(result.MRData.RaceTable.Races);
+          setIsLoading(false);
+        },
+        (error) => {
+          setIsLoading(false);
+          setError(error);
+        }
+      );
+  }, []);
+
+  const Result = ({ position, firstName, surname, constructor }) => (
     <View>
-      <Text>Test</Text>
+      <Text style={styles.result}>
+        {position} - {firstName} {surname} : {constructor}
+      </Text>
     </View>
+  );
+
+  const renderItem = ({ item }) => {
+    return (
+      <Result
+        position={item.position}
+        firstName={item.Driver.givenName}
+        surname={item.Driver.familyName}
+        constructor={item.Constructor.name}
+      />
+    );
+  };
+
+  if (isLoading) {
+    return (
+      <ActivityIndicator
+        style={{ marginTop: 20 }}
+        size="large"
+        color="#039dfc"
+      />
+    );
+  }
+
+  if (error) {
+    return (
+      <View>
+        <Text>Error: {error.message}</Text>
+      </View>
+    );
+  }
+
+  return (
+    <SafeAreaView style={styles.resultList}>
+      <View style={styles.heading}>
+        <Text>
+          {results[0].season} {results[0].raceName}
+        </Text>
+      </View>
+      <FlatList
+        data={resultsArray}
+        renderItem={renderItem}
+        keyExtractor={(Result) => Result.Driver.driverId}
+      />
+    </SafeAreaView>
   );
 };
 
---
+++ screens/ResultsScreen/style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  resultList: {
+    marginLeft: 10,
+    marginTop: 10,
+  },
+
+  result: {
+    marginTop: 10,
+    marginBottom: 5,
+  },
+
+  heading: {
+    display: "flex",
+    alignItems: "center",
+  },
+});
