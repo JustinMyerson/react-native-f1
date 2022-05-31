9d699f99b2fe17e57adffad45cafa6fe06ce3e16
JustinMyerson <100346942+JustinMyerson@users.noreply.github.com>
2022-05-31 12:13:40 +0200

    Create Driver Screen

---
+++ screens/DriverScreen/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import { styles } from "./style";
+
+function DriverScreen() {
+  return (
+    <View style={styles.driverView}>
+      <Text>Text</Text>
+    </View>
+  );
+}
+
+export default DriverScreen;
